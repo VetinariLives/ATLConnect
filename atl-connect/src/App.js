f26da465aa7c90548c54cd177e5fdbe0703ce2b0
masrysarah11 <masrysarah11@gmail.com>
2024-02-10 15:37:37 -0500

    basic navbar implementation

---
+++ atl-connect/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import Navbar from './navbar.js';
+
+function App() {
+  return (
+    <div className="App">
+      <div className = "Navbar">
+        <Navbar />
+        <div id = "home">
+          
+        </div>
+        <div id = "about">
+
+        </div>
+        <div id = "map">
+
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
