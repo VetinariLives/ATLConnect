cdd27054b2108e9c58f3cc818c889deed82fea70
Talha Ansari <93846591+VetinariLives@users.noreply.github.com>
2024-02-11 07:38:03 -0500

    idk panic

--- atl-connect/src/Main.js
+++ atl-connect/src/Main.js
@@ -8,46 +8,10 @@ function Main() {
                 <div class="box-main">
                     <div class="firstHalf">
                         <h1 class="text-big">
-                            blank title
+                            Mission Statement:
                         </h1>
                         <p class="text-small">
-                            blank blank blank
-                        </p>
-                    </div>
-                </div>
-            </section>
-            <section class="section">
-                <div class="box-main">
-                    <div class="secondHalf">
-                        <h1 class="text-big" id="program">
-                            blank title
-                        </h1>
-                        <p class="text-small">
-                            blank
-                        </p>
-                    </div>
-                </div>
-            </section>
-            <section class="section">
-                <div class="box-main">
-                    <div class="secondHalf">
-                        <h1 class="text-big" id="program">
-                            blank title
-                        </h1>
-                        <p class="text-small">
-                            blank blank blank
-                        </p>
-                    </div>
-                </div>
-            </section>
-            <section class="section">
-                <div class="box-main">
-                    <div class="secondHalf">
-                        <h1 class="text-big" id="program">
-                            blank title
-                        </h1>
-                        <p class="text-small">
-                            blank blank blank
+                            ATLConnect is ATL's hub for community resources, aggregating information about food, housing, restaraunts, education, events and more! Users can suggest new resources and events in the suggestions form.
                         </p>
                     </div>
                 </div>
